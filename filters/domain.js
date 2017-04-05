8ae3f3b708f594e365cee8bcd39ee3dddc870b09
Carlo Alberto Degli Atti <lordkada@gmail.com>
2017-04-05 18:27:28 +0200

    incoming requests for unknown domains don't break server anymore

--- filters/domain.js
+++ filters/domain.js
@@ -1,7 +1,11 @@
 function domain(domains) {
     return function(req, res, next) {
         let domain_uuid = req.get("domain_uuid") || Object.keys(domains)[0];
-        req.domain = domains[domain_uuid];
+        req.domain = domains[domain_uuid] || {
+            uuid: domain_uuid,
+            tags:[],
+            files: []
+        };
         next();
     }
 }
